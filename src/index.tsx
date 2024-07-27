88b5bd33808144b91e271b5252613a5185c962b7
EdroVolt <ahmededrees30@gmail.com>
2024-07-27 14:08:14 +0300

    config: add vercel analytics to monitor website visits

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { App } from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 import * as serviceWorker from "./serviceWorker";
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.render(
   <React.StrictMode>
